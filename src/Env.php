f113f1c4d1514290005a3b2e17aa7735823a3c06
Jordy Manner <jordy.manner@milkcreation.fr>
2021-10-01 09:21:15 +0200

    feat/v1.0.8 Add Env MergeVars capability

--- src/Env.php
+++ src/Env.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Pollen\Support;
 
 use Dotenv\Dotenv;
-use Illuminate\Support\Env as BaseEnv;
+use Dotenv\Repository\RepositoryInterface;
+use Dotenv\Repository\RepositoryBuilder;
+use PhpOption\Option;
 
-class Env extends BaseEnv
+class Env
 {
     /**
      * Environment loader instance.
@@ -16,14 +18,86 @@ class Env extends BaseEnv
     protected static ?Dotenv $loader = null;
 
     /**
-     * @param string $dir
+     * Environment repository instance.
+     * @var RepositoryInterface|null
+     */
+    protected static ?RepositoryInterface $repository = null;
+
+    /**
+     * Merge Vars Bag instance.
+     * @var ParamsBag|null
+     */
+    protected static ?ParamsBag $mergeVarsBag = null;
+
+    /**
+     * Gets the value of an environment variable.
+     *
+     * @param string $key
+     * @param null $default
+     *
+     * @return string|bool|null
+     */
+    public static function get(string $key, $default = null)
+    {
+        return Option::fromValue(static::getRepository()->get($key))
+            ->map(function ($value) {
+                switch (strtolower($value)) {
+                    case 'true':
+                    case '(true)':
+                        return true;
+                    case 'false':
+                    case '(false)':
+                        return false;
+                    case 'empty':
+                    case '(empty)':
+                        return '';
+                    case 'null':
+                    case '(null)':
+                        return null;
+                }
+
+                if (preg_match('/\A([\'"])(.*)\1\z/', $value, $matches)) {
+                    $value = $matches[2];
+                }
+
+                if (static::mergeVarsBag()->count()) {
+                    $value = static::getMergedVarsValue($value);
+                }
+
+                return $value;
+            })
+            ->getOrCall(function () use ($default) {
+                return value($default);
+            });
+    }
+
+    /**
+     * Gets the environment repository instance.
+     *
+     * @return RepositoryInterface
+     */
+    public static function getRepository(): RepositoryInterface
+    {
+        if (static::$repository === null) {
+            $builder = RepositoryBuilder::createWithDefaultAdapters();
+
+            static::$repository = $builder->immutable()->make();
+        }
+
+        return static::$repository;
+    }
+
+    /**
+     * Loads the environment variables from a path.
+     *
+     * @param string $path
      *
      * @return Dotenv
      */
-    public static function load(string $dir): Dotenv
+    public static function load(string $path): Dotenv
     {
         if (static::$loader === null) {
-            static::$loader = Dotenv::createImmutable($dir, ['.env', '.env.local'], false);
+            static::$loader = Dotenv::create(static::getRepository(), $path, ['.env', '.env.local'], false);
             static::$loader->safeLoad();
         }
 
@@ -58,5 +132,52 @@ class Env extends BaseEnv
     public static function inStaging(): bool
     {
         return static::get('APP_ENV') === 'staging';
+    }
+
+    /**
+     * Gets the merged vars bag instance.
+     *
+     * @return ParamsBag
+     *
+     * @throws \InvalidArgumentException
+     */
+    public static function mergeVarsBag(): ParamsBag
+    {
+        if (static::$mergeVarsBag === null) {
+            static::$mergeVarsBag = new ParamsBag();
+        }
+
+        return static::$mergeVarsBag;
+    }
+
+    /**
+     * Gets a value with merged vars replacement.
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    private static function getMergedVarsValue(string $value): string
+    {
+        if (preg_match_all('/([^%%]*)%%(.*?)%%([^%%]*)?/', $value, $matches)) {
+            $value = '';
+            foreach ($matches[2] as $i => $tag) {
+                $value .= $matches[1][$i] . static::mergeVarsBag()->get($tag, $matches[2][$i]) . $matches[3][$i];
+            }
+        }
+
+        return $value;
+    }
+
+    /**
+     * Set merge vars.
+     *
+     * @param array $mergeVars
+     *
+     * @return void
+     */
+    public static function setMergeVars(array $mergeVars): void
+    {
+        static::mergeVarsBag()->set($mergeVars);
     }
 }
